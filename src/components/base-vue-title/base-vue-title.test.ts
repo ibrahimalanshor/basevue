90c44156e24e42eff1de6f1d1aefac279ea84c43
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-09-17 13:02:38 +0700

    feat: base title heading elemnt

--- src/components/base-vue-title/base-vue-title.test.ts
+++ src/components/base-vue-title/base-vue-title.test.ts
@@ -3,6 +3,12 @@ import { describe, expect, test } from 'vitest';
 import BaseVueTitle from './base-vue-title.vue';
 
 describe.only('title component', () => {
+  test('should render heading element', () => {
+    const wrapper = mount(BaseVueTitle);
+
+    expect(wrapper.find('h1')).toBeTruthy();
+  });
+
   test('should render title default slot', () => {
     const wrapper = mount(BaseVueTitle, {
       slots: {
