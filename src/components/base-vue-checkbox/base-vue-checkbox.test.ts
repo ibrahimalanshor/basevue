688d7c2065dee72ddeff5914a809075d475454f5
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-09-19 06:59:13 +0700

    fat: checkbox label

--- src/components/base-vue-checkbox/base-vue-checkbox.test.ts
+++ src/components/base-vue-checkbox/base-vue-checkbox.test.ts
@@ -40,7 +40,7 @@ describe.only('checkbox test', () => {
   test('should have default checkbox class', () => {
     const wrapper = mount(BaseVueChekbox);
 
-    expect(wrapper.props('inputClass')).toBeUndefined();
+    expect(wrapper.props('checkboxClass')).toBeUndefined();
   });
   test('should have checkbox class', () => {
     const wrapper = mount(BaseVueChekbox, {
@@ -52,4 +52,47 @@ describe.only('checkbox test', () => {
     expect(wrapper.find('input[type=checkbox]').classes('h-4')).toBe(true);
     expect(wrapper.find('input[type=checkbox]').classes('border')).toBe(true);
   });
+
+  // label
+  test('should have label element', () => {
+    const wrapper = mount(BaseVueChekbox, {
+      props: {
+        id: 'select',
+        label: 'Select',
+      },
+    });
+
+    expect(wrapper.find('label').exists()).toBe(true);
+    expect(wrapper.find('label').text()).toEqual('Select');
+    expect(wrapper.find('label').attributes('for')).toEqual('select');
+  });
+  test('should have default label class', () => {
+    const wrapper = mount(BaseVueChekbox);
+
+    expect(wrapper.props('labelClass')).toBeUndefined();
+  });
+  test('should have label class', () => {
+    const wrapper = mount(BaseVueChekbox, {
+      props: {
+        labelClass: 'font-bold text-sm',
+      },
+    });
+
+    expect(wrapper.find('label').classes('font-bold')).toBe(true);
+    expect(wrapper.find('label').classes('text-sm')).toBe(true);
+  });
+  // test('should check checkbox when label clicked', async () => {
+  //   const wrapper = mount(BaseVueChekbox, {
+  //     props: {
+  //       id: 'select'
+  //     },
+  //   });
+
+  //   await wrapper.find('label').trigger('click')
+
+  //   console.log(wrapper.find('input[type=checkbox]').attributes())
+  //   console.log(wrapper.find('input[type=checkbox]').element.attributes)
+
+  //   expect(wrapper.find('input[type=checkbox]').attributes('checked')).toBe(true)
+  // })
 });
--- src/components/base-vue-checkbox/base-vue-checkbox.vue
+++ src/components/base-vue-checkbox/base-vue-checkbox.vue
@@ -6,12 +6,16 @@ export default defineComponent({
   props: {
     wrapperClass: String,
     checkboxClass: String,
+    labelClass: String,
+    label: String,
+    id: String,
   },
 });
 </script>
 
 <template>
   <div :class="wrapperClass">
-    <input type="checkbox" :class="checkboxClass" />
+    <input :id="id" type="checkbox" :class="checkboxClass" />
+    <label :for="id" :class="labelClass">{{ label }}</label>
   </div>
 </template>
