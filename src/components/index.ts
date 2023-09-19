0d14de5d8398e4c7e7b2d97a406d67e2ae901c39
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-09-20 04:09:05 +0700

    feat: component entry point

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as BaseVueButton } from './base-vue-button/base-vue-button.vue';
+export { default as BaseVueCheckbox } from './base-vue-checkbox/base-vue-checkbox.vue';
+export { default as BaseVueInput } from './base-vue-input/base-vue-input.vue';
+export { default as BaseVueList } from './base-vue-list/base-vue-list.vue';
+export { default as BaseVueText } from './base-vue-text/base-vue-text.vue';
+export { default as BaseVueTitle } from './base-vue-title/base-vue-title.vue';
