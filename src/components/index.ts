8643b90d5115b442cceb182074ae1bd403dfd2a5
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-09-24 14:37:21 +0700

    fix: export dropdown

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as PlainVueInput } from './plain-vue-input/plain-vue-input.vue'
 export { default as PlainVueList } from './plain-vue-list/plain-vue-list.vue';
 export { default as PlainVueText } from './plain-vue-text/plain-vue-text.vue';
 export { default as PlainVueTitle } from './plain-vue-title/plain-vue-title.vue';
+export { default as PlainVueDropdown } from './plain-vue-dropdown/plain-vue-dropdown.vue';
